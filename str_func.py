5bceb64a4d76d1c332c9fc657961fd905c022213
SilverAnton <cu4hawk@gmail.com>
2024-01-26 15:00:04 +0700

    add to str_func.py new foo

--- str_func.py
+++ str_func.py
@@ -2,3 +2,13 @@ def all_caps(value):
     return str(value).upper()
 
 print(all_caps('приветик'))
+
+def title_first(value):
+    """функция возвращает строки с заглавной буквы"""
+    return str(value).title()
+
+print(title_first('develop'))
+
+
+
+
